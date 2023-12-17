7540da18682356d999518d6394d7c006f78af354
Eric Ogbekene <ericogbekene@icloud.com>
2023-12-17 06:16:09 -0800

    fix selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,9 +21,9 @@ void selection_sort(int *array, size_t size)
 
 			if (min != i)
 			{
-				temp = array[min];
-				array[min] = array[i];
-				array[i] = temp;
+				temp = array[i];
+				array[i] = array[min];
+				array[min] = temp;
 				print_array(array, size);
 			}
 		}
